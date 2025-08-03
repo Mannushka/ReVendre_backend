34ff69879ab2b27dd27c93086fc1367d214c66ad
Mannushka <zaimar1910@gmail.com>
2025-08-03 11:58:13 +0800

    configure dotenv, remove logs

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import { AppDataSource } from "./data-source";
 import { User } from "./entity/User";
 // import * as dotenv from "dotenv";
 // dotenv.config(); // Loads .env into process.env
+require("dotenv").config();
 
 const PORT = 3000;
 AppDataSource.initialize()
@@ -23,9 +24,6 @@ AppDataSource.initialize()
     console.log(
       "Here you can setup and run express / fastify / any other framework."
     );
-
-    console.log("DB_DATABASE:", process.env.DB_DATABASE);
-    console.log("DB_HOST", process.env.DB_HOST);
 
     app.listen(PORT, () => {
       console.log(`Server running on http://localhost:${PORT}`);
