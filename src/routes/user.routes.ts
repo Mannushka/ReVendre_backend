ab1a99303fc7d958e8eaf7224e0e7ebbf6f1739c
Mannushka <zaimar1910@gmail.com>
2025-08-03 21:29:33 +0800

    add route for fetching one user

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import { UserController } from "../controllers/user.controller";
+import { BaseController } from "../controllers/base.controller";
 
 const router = express.Router();
 const userController = new UserController();
 
 router.get("/", userController.getAll.bind(userController));
+router.get("/:id", userController.getOne.bind(userController));
 router.post("/", userController.createUser.bind(userController));
 
 export default router;
