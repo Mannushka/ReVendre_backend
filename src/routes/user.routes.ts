ef4abf90757731b87aec36678f18a8ab678326b0
Mannushka <zaimar1910@gmail.com>
2025-08-03 18:41:19 +0800

    adjust the route accrording to the controller

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -4,7 +4,7 @@ import { UserController } from "../controllers/user.controller";
 const router = express.Router();
 const userController = new UserController();
 
-router.get("/", userController.all.bind(userController));
-router.post("/", userController.save.bind(userController));
+router.get("/", userController.getAll.bind(userController));
+router.post("/", userController.createUser.bind(userController));
 
 export default router;
