75e6560e96273c5e2cce9ed0a62876fd7e079986
Mannushka <zaimar1910@gmail.com>
2025-08-05 19:36:47 +0800

    provide validation schema for  creating of a new user route

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,12 +1,18 @@
 import express from "express";
 import { UserController } from "../controllers/user.controller";
 import { BaseController } from "../controllers/base.controller";
+import { checkSchema } from "express-validator";
+import { createUserValidationSchema } from "../utils/validationSchema";
 
 const router = express.Router();
 const userController = new UserController();
 
 router.get("/", userController.getAll.bind(userController));
 router.get("/:id", userController.getOne.bind(userController));
-router.post("/", userController.createUser.bind(userController));
+router.post(
+  "/",
+  checkSchema(createUserValidationSchema),
+  userController.createUser.bind(userController)
+);
 
 export default router;
