6013682baf93653d7f6dd0ff90188575397a9ae7
Mannushka <zaimar1910@gmail.com>
2025-08-05 19:34:54 +0800

    implement input validation for creating a new user

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -3,7 +3,8 @@ import { BaseController } from "./base.controller";
 import { AppDataSource } from "../data-source";
 import { User } from "../entity/User";
 import { QueryFailedError } from "typeorm";
-import { Validate } from "class-validator";
+import { checkSchema, matchedData, validationResult } from "express-validator";
+import { createUserValidationSchema } from "../utils/validationSchema";
 
 export class UserController extends BaseController<User> {
   // private userRepository = AppDataSource.getRepository(User);
@@ -12,16 +13,21 @@ export class UserController extends BaseController<User> {
   }
 
   async createUser(request: Request, response: Response) {
+    const errors = validationResult(request);
+    if (!errors.isEmpty()) {
+      return response.status(400).json({
+        errors: errors.array(),
+      });
+    }
+
     const { userName, email, phoneNumber, imageUrl } = request.body;
 
-    //TO DO -> DATA TYPE VALIDATION!!!
-
     try {
-      if (!userName || !email) {
-        return response
-          .status(400)
-          .json({ message: "User name and email are required" });
-      }
+      // if (!userName || !email) {
+      //   return response
+      //     .status(400)
+      //     .json({ message: "User name and email are required" });
+      // }
 
       const user = this.repository.create({
         userName,
