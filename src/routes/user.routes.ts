d1448b2abc1a07048cfef0021e245d2c7da3aa7b
Mannushka <zaimar1910@gmail.com>
2025-08-11 23:23:40 +0800

    add user id validation for get one by id

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,14 +1,20 @@
 import express from "express";
 import { UserController } from "../controllers/user.controller";
-import { BaseController } from "../controllers/base.controller";
 import { checkSchema } from "express-validator";
-import { createUserValidationSchema } from "../utils/validationSchema";
+import {
+  createUserValidationSchema,
+  getOneByIdValidationSchema,
+} from "../utils/validationSchema";
 
 const router = express.Router();
 const userController = new UserController();
 
 router.get("/", userController.getAll.bind(userController));
-router.get("/:id", userController.getOne.bind(userController));
+router.get(
+  "/:id",
+  getOneByIdValidationSchema,
+  userController.getOne.bind(userController)
+);
 router.post(
   "/",
   checkSchema(createUserValidationSchema),
