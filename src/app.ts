f22195a3c4c45881cfef31039573d0c8363ad319
Mannushka <zaimar1910@gmail.com>
2025-08-17 22:39:24 +0800

    add cors

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,9 @@ import "reflect-metadata";
 import userRoutes from "./routes/user.routes";
 
 const app = express();
+const cors = require("cors");
+
+app.use(cors({ origin: true, credentials: true })); // limit allowed origins later!
 app.use(express.json());
 
 app.use("/users", userRoutes);
