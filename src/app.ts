3e27654e4664023a4559838aec323caddbd507a5
Mannushka <zaimar1910@gmail.com>
2025-08-30 23:20:05 +0800

    add clerk middleware

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,14 @@
 import express from "express";
 import "reflect-metadata";
 import userRoutes from "./routes/user.routes";
+import { clerkMiddleware } from "@clerk/express";
 
 const app = express();
 const cors = require("cors");
 
 app.use(cors({ origin: true, credentials: true })); // limit allowed origins later!
 app.use(express.json());
-
+app.use(clerkMiddleware());
 app.use("/users", userRoutes);
 // server.use("/users", userRoutes); //later add routes here
 
